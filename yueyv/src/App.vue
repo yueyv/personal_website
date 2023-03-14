e52c6750eac01aa07daf61052ff062839939a3ba
ixzy1999 <2071766850@qq.com>
2023-03-14 17:33:22 +0800

    beforevite

--- yueyv/src/App.vue
+++ yueyv/src/App.vue
@@ -1,23 +1,30 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
 <template>
-  <!-- <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav> -->
-  <router-view/>
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<style lang="scss">
-*{
-  margin: 0;
-  padding: 0;
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
 }
-#app,body,html {
-  height: 100%;
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
 }
-
-.ant-layout {
-  display: flex;
-  width: 100%;
-  min-height: 100%;
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
--- yueyv/src/main.ts
+++ yueyv/src/main.ts
@@ -1,10 +1,5 @@
 import { createApp } from 'vue'
+import './style.css'
 import App from './App.vue'
-import router from './router'
-import Antd from 'ant-design-vue';
-import "ant-design-vue/dist/antd.css"; // or 'ant-design-vue/dist/antd.less'
 
-const app = createApp(App)
-app.use(Antd)
-app.use(router).mount('#app')
-    
+createApp(App).mount('#app')
