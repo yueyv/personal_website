c569e00c445a7968e254b90dcdac79c1b4293764
yueyv <yueyvlunhui@gmail.com>
2023-05-21 18:01:12 +0800

    update

--- test/axios.js
+++
@@ -1,8 +0,0 @@
-const axios = require('axios')
-axios.get("http://localhost:3000/")
-    .then((response) =>{
-        console.log(response.data)
-    })
-    .catch(function (error) { // 请求失败处理
-        console.log(error);
-    });
